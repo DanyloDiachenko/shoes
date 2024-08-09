740a1d9319758bbb500ae03849d81ca4d64c1fc1
Danylo <107757498+DanyloDiachenko@users.noreply.github.com>
2024-08-09 18:24:41 +0300

    new arrivals

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import { BannerGroup } from "@/components/pageComponents/home/BannerGroup";
+import { NewArrivals } from "@/components/pageComponents/home/NewArrivals";
 import { Services } from "@/components/pageComponents/home/Services";
 import { Slider } from "@/components/pageComponents/home/Slider";
 
@@ -8,6 +9,7 @@ const Home = () => {
             <Slider />
             <BannerGroup />
             <Services />
+            <NewArrivals />
         </>
     );
 };
