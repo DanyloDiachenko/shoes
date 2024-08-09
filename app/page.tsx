4d5dbe8507cd800a40a31b6bb00a01538328ac56
Danylo <107757498+DanyloDiachenko@users.noreply.github.com>
2024-08-09 18:58:07 +0300

    content

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import { BannerGroup } from "@/components/pageComponents/home/BannerGroup";
+import { BlogPosts } from "@/components/pageComponents/home/BlogPosts";
 import { Categories } from "@/components/pageComponents/home/Categories";
 import { NewArrivals } from "@/components/pageComponents/home/NewArrivals";
+import { NewDeals } from "@/components/pageComponents/home/NewDeals";
 import { Services } from "@/components/pageComponents/home/Services";
 import { Slider } from "@/components/pageComponents/home/Slider";
 import { TopSellingProducts } from "@/components/pageComponents/home/TopSellingProducts";
@@ -13,7 +15,11 @@ const Home = () => {
             <Services />
             <NewArrivals />
             <Categories />
+            <div className="mb-4"></div>
             <TopSellingProducts />
+            <div className="mb-5"></div>
+            <NewDeals />
+            <BlogPosts />
         </>
     );
 };
