1219566ec6b051ebe8e2e1d1f1fc4da3b0822bbb
Danylo <107757498+DanyloDiachenko@users.noreply.github.com>
2024-08-15 12:28:27 +0300

    header navigation

--- components/common/Header/HeaderMiddle/HeaderLeft/index.tsx
+++ components/common/Header/HeaderMiddle/HeaderLeft/index.tsx
@@ -1,101 +1,40 @@
 import Link from "next/link";
 import headerNavigation from "../../../../../data/headerNavigation.json";
+import styles from "./styles.module.scss";
+import { RxHamburgerMenu } from "react-icons/rx";
+import { IoIosArrowDown } from "react-icons/io";
 
 export const HeaderLeft = () => {
     return (
-        <div className="header-left">
-            <button className="mobile-menu-toggler">
-                <span className="sr-only">Toggle mobile menu</span>
-                <i className="icon-bars"></i>
+        <div className={styles.headerLeft}>
+            <button className={styles.mobileMenuToggler}>
+                <span className="hidden">Toggle mobile menu</span>
+                <RxHamburgerMenu />
             </button>
-            <Link href="/" className="logo">
+            <Link href="/" className={styles.logo}>
                 <img
-                    src="/images/logo.png"
+                    src="/images/icons/logo.png"
                     alt="Molla Logo"
                     width="105"
                     height="25"
                 />
             </Link>
-            <nav className="main-nav">
-                <ul className="menu sf-arrows">
+            <nav className={styles.mainNav}>
+                <ul className={styles.menu}>
                     {headerNavigation.map((item, index) => (
-                        <li key={index}>
-                            <Link href={item.url} className="sf-with-ul">
-                                {item.title}
+                        <li key={index} className={styles.linksWrapper}>
+                            <Link href={item.url} className={styles.mainLink}>
+                                {item.title} <IoIosArrowDown />
                             </Link>
-                            {item.rightImageSrc ? (
-                                <div className="megamenu megamenu-sm">
-                                    <div className="row no-gutters">
-                                        <div className="col-md-6">
-                                            <div className="menu-col">
-                                                <div className="menu-title">
-                                                    {item.subtitle}
-                                                </div>
-                                                <ul>
-                                                    {item.sublinks.map(
-                                                        (sublink, index) => (
-                                                            <li key={index}>
-                                                                <Link
-                                                                    href={
-                                                                        sublink.url
-                                                                    }
-                                                                >
-                                                                    <span>
-                                                                        {
-                                                                            sublink.title
-                                                                        }
-                                                                        {sublink.isNew && (
-                                                                            <span className="tip tip-new">
-                                                                                New
-                                                                            </span>
-                                                                        )}
-                                                                    </span>
-                                                                </Link>
-                                                            </li>
-                                                        )
-                                                    )}
-                                                </ul>
-                                            </div>
-                                        </div>
-                                        <div className="col-md-6">
-                                            <div className="banner banner-overlay">
-                                                <Link href={item.rightImageUrl}>
-                                                    <img
-                                                        src={item.rightImageSrc}
-                                                        alt="Banner"
-                                                    />
-
-                                                    <div className="banner-content banner-content-bottom">
-                                                        <div className="banner-title text-white">
-                                                            {
-                                                                item.rightImageTitle
-                                                            }
-                                                            <br />
-                                                            <span>
-                                                                <strong>
-                                                                    {
-                                                                        item.rightImageTitleStrong
-                                                                    }
-                                                                </strong>
-                                                            </span>
-                                                        </div>
-                                                    </div>
-                                                </Link>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            ) : (
-                                <ul>
-                                    {item.sublinks.map((sublink, index) => (
-                                        <li key={index}>
-                                            <Link href={sublink.url}>
-                                                {sublink.title}
-                                            </Link>
-                                        </li>
-                                    ))}
-                                </ul>
-                            )}
+                            <ul className={styles.sublinks}>
+                                {item.sublinks.map((sublink, index) => (
+                                    <li key={index}>
+                                        <Link href={sublink.url}>
+                                            {sublink.title}
+                                        </Link>
+                                    </li>
+                                ))}
+                            </ul>
                         </li>
                     ))}
                 </ul>
--- components/common/Header/HeaderMiddle/index.tsx
+++ components/common/Header/HeaderMiddle/index.tsx
@@ -1,11 +1,12 @@
 import Link from "next/link";
 import { HeaderLeft } from "./HeaderLeft";
 import { HeaderRight } from "./HeaderRight";
+import styles from "./styles.module.scss";
 
 export const HeaderMiddle = () => {
     return (
-        <div className="header-middle sticky-header">
-            <div className="container">
+        <div className={`${styles.headerMiddle} sticky-header`}>
+            <div className={`container ${styles.container}`}>
                 <HeaderLeft />
                 <HeaderRight />
             </div>
