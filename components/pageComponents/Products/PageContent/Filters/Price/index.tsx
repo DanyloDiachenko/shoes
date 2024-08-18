ce8b08c9e12a9dbcce203ea1915d2ae01529aad0
Danylo <107757498+DanyloDiachenko@users.noreply.github.com>
2024-08-18 18:41:34 +0300

    price filter & input range styles

--- components/pageComponents/Products/PageContent/Filters/Price/index.tsx
+++ components/pageComponents/Products/PageContent/Filters/Price/index.tsx
@@ -4,9 +4,12 @@ import { IoIosArrowDown } from "react-icons/io";
 import styles from "./styles.module.scss";
 import { useState } from "react";
 import { Checkbox } from "@/components/UI/Checkbox";
+import InputRange, { Range } from "react-input-range";
+/* import "react-input-range/lib/css/index.css"; */
 
 export const Price = () => {
     const [isOpened, setIsOpened] = useState(true);
+    const [price, setPrice] = useState<Range>({ min: 0, max: 1000 });
 
     return (
         <div className={styles.widget}>
@@ -31,8 +34,32 @@ export const Price = () => {
                 <div className={styles.widgetBody}>
                     <div className={styles.filterPrice}>
                         <div className={styles.text}>
-                            Price Range: <span>$0 - $600</span>
+                            Price Range:{" "}
+                            <span>
+                                ${price.min} - ${price.max}
+                            </span>
                         </div>
+                        <InputRange
+                            value={price}
+                            onChange={(newPrice) => setPrice(newPrice as Range)}
+                            minValue={0}
+                            maxValue={1000}
+                            step={50}
+                            formatLabel={(price) => `$${price}`}
+                            classNames={{
+                                track: styles.inputRangeTrack,
+                                activeTrack: styles.inputRangeTrackActive,
+                                disabledInputRange: styles.inputRangeDisabled,
+                                inputRange: styles.inputRange,
+                                labelContainer: styles.inputRangeLabelContainer,
+                                maxLabel: styles.inputRangeLabelMax,
+                                minLabel: styles.inputRangeLabelMin,
+                                slider: styles.inputRangeSlider,
+                                sliderContainer:
+                                    styles.inputRangeSliderContainer,
+                                valueLabel: styles.inputRangeLabelValue,
+                            }}
+                        />
                     </div>
                 </div>
             </div>
