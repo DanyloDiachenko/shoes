e35b726c08439a086a8f729406446666174befbe
Danylo <107757498+DanyloDiachenko@users.noreply.github.com>
2024-08-24 00:00:01 +0300

    img

--- components/pageComponents/Product/DetailsTop/LeftColumn/index.tsx
+++ components/pageComponents/Product/DetailsTop/LeftColumn/index.tsx
@@ -1,8 +1,20 @@
+"use client";
+
 import Link from "next/link";
 import styles from "./styles.module.scss";
 import { FaExpandArrowsAlt } from "react-icons/fa";
+import { useState } from "react";
+
+const productImages: string[] = [
+    "/images/banners/product-details.png",
+    "/images/banners/product-details.png",
+    "/images/banners/product-details.png",
+    "/images/banners/product-details.png",
+];
 
 export const ProductDetailsTopLeftColumn = () => {
+    const [activeImage, setActiveImage] = useState<string>(productImages[0]);
+
     return (
         <div className={styles.columnLeft}>
             <div className={styles.gallery}>
@@ -10,7 +22,7 @@ export const ProductDetailsTopLeftColumn = () => {
                     <figure className={styles.mainImage}>
                         <img
                             id="product-zoom"
-                            src="/images/banners/product-details.png"
+                            src={activeImage}
                             alt="product image"
                         />
                         <Link href="#" className={styles.productGalleryLink}>
@@ -18,33 +30,18 @@ export const ProductDetailsTopLeftColumn = () => {
                         </Link>
                     </figure>
                     <div className={styles.galleryItems}>
-                        <Link
-                            className={`${styles.galleryItem} ${styles.active}`}
-                            href="#"
-                        >
-                            <img
-                                src="/images/banners/product-details.png"
-                                alt="product side"
-                            />
-                        </Link>
-                        <Link className={`${styles.galleryItem}`} href="#">
-                            <img
-                                src="/images/banners/product-details.png"
-                                alt="product side"
-                            />
-                        </Link>
-                        <Link className={`${styles.galleryItem}`} href="#">
-                            <img
-                                src="/images/banners/product-details.png"
-                                alt="product side"
-                            />
-                        </Link>
-                        <Link className={`${styles.galleryItem}`} href="#">
-                            <img
-                                src="/images/banners/product-details.png"
-                                alt="product side"
-                            />
-                        </Link>
+                        {productImages.map((image, index) => (
+                            <Link
+                                className={`${styles.galleryItem} ${
+                                    image === activeImage ? styles.active : ""
+                                }`}
+                                href="#"
+                                onClick={() => setActiveImage(image)}
+                                key={index}
+                            >
+                                <img src={image} alt="product side" />
+                            </Link>
+                        ))}
                     </div>
                 </div>
             </div>
