aedc19ac9088921807b6271c45abddd884463659
Danylo <107757498+DanyloDiachenko@users.noreply.github.com>
2024-08-25 13:06:04 +0300

    reviews styles

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 1. product details reviews tab + styles
+   ![alt text](image.png)
+   ![alt text](image-1.png)
--- components/pageComponents/Product/DetailsBottom/TabsContent/Reviews/index.tsx
+++ components/pageComponents/Product/DetailsBottom/TabsContent/Reviews/index.tsx
@@ -1,92 +1,42 @@
+import Link from "next/link";
 import styles from "./styles.module.scss";
+import { getRating } from "@/helpers/getRating";
+import { AiOutlineLike, AiOutlineDislike } from "react-icons/ai";
 
 export const Reviews = () => {
     return (
-        <div
-            className="tab-pane fade"
-            id="product-review-tab"
-            role="tabpanel"
-            aria-labelledby="product-review-link"
-        >
-            <div className="reviews">
-                <h3>Reviews (2)</h3>
-                <div className="review">
-                    <div className="row no-gutters">
-                        <div className="col-auto">
-                            <h4>
-                                <a href="#">Samanta J.</a>
-                            </h4>
-                            <div className="ratings-container">
-                                <div className="ratings">
-                                    <div className="ratings-val"></div>
-                                </div>
-                            </div>
-                            <span className="review-date">6 days ago</span>
+        <div className={styles.reviews}>
+            <h3 className={styles.title}>Reviews (2)</h3>
+            <div className={styles.review}>
+                <div className={`row ${styles.row}`}>
+                    <div className={styles.leftColumn}>
+                        <h4>Samanta J.</h4>
+                        <div className={styles.ratingsContainer}>
+                            {getRating(4)}
                         </div>
-                        <div className="col">
-                            <h4>Good, perfect size</h4>
-
-                            <div className="review-content">
-                                <p>
-                                    Lorem ipsum dolor sit amet, consectetur
-                                    adipisicing elit. Ducimus cum dolores
-                                    assumenda asperiores facilis porro
-                                    reprehenderit animi culpa atque blanditiis
-                                    commodi perspiciatis doloremque, possimus,
-                                    explicabo, autem fugit beatae quae voluptas!
-                                </p>
-                            </div>
-
-                            <div className="review-action">
-                                <a href="#">
-                                    <i className="icon-thumbs-up"></i>
-                                    Helpful (2)
-                                </a>
-                                <a href="#">
-                                    <i className="icon-thumbs-down"></i>
-                                    Unhelpful (0)
-                                </a>
-                            </div>
-                        </div>
+                        <span className={styles.raviewDate}>6 days ago</span>
                     </div>
-                </div>
-
-                <div className="review">
-                    <div className="row no-gutters">
-                        <div className="col-auto">
-                            <h4>
-                                <a href="#">John Doe</a>
-                            </h4>
-                            <div className="ratings-container">
-                                <div className="ratings">
-                                    <div className="ratings-val"></div>
-                                </div>
-                            </div>
-                            <span className="review-date">5 days ago</span>
+                    <div className={styles.mainColumn}>
+                        <h4>Good, perfect size</h4>
+                        <div className={styles.description}>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur
+                                adipisicing elit. Ducimus cum dolores assumenda
+                                asperiores facilis porro reprehenderit animi
+                                culpa atque blanditiis commodi perspiciatis
+                                doloremque, possimus, explicabo, autem fugit
+                                beatae quae voluptas!
+                            </p>
                         </div>
-                        <div className="col">
-                            <h4>Very good</h4>
-
-                            <div className="review-content">
-                                <p>
-                                    Sed, molestias, tempore? Ex dolor esse iure
-                                    hic veniam laborum blanditiis laudantium
-                                    iste amet. Cum non voluptate eos enim, ab
-                                    cumque nam, modi, quas iure illum
-                                    repellendus, blanditiis perspiciatis beatae!
-                                </p>
-                            </div>
-
-                            <div className="review-action">
-                                <a href="#">
-                                    <i className="icon-thumbs-up"></i>
-                                    Helpful (0)
-                                </a>
-                                <a href="#">
-                                    <i className="icon-thumbs-down"></i>
-                                    Unhelpful (0)
-                                </a>
-                            </div>
+                        <div className={styles.actions}>
+                            <Link href="#">
+                                <AiOutlineLike />
+                                <span>Helpful (2)</span>
+                            </Link>
+                            <Link href="#">
+                                <AiOutlineDislike />
+                                <span>Unhelpful (0)</span>
+                            </Link>
                         </div>
                     </div>
                 </div>
