89d408baf70072265a6b07501568f15c6d57dc98
Danylo <107757498+DanyloDiachenko@users.noreply.github.com>
2024-09-02 23:57:13 +0300

    get sizes

---
+++ app/api/sizes.ts
@@ -0,0 +1,15 @@
+import { IProductSize } from "@/interfaces/product.interface";
+
+export const getSizes = async (): Promise<IProductSize[]> => {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/sizes`, {
+        cache: "no-cache",
+    });
+
+    if (!res.ok) {
+        throw new Error("Failed to fetch data");
+    }
+
+    const data = await res.json();
+
+    return data;
+};
