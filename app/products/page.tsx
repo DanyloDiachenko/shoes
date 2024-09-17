8559a2867fa17685e886efbbb6cb37660c43e783
Danylo <107757498+DanyloDiachenko@users.noreply.github.com>
2024-09-17 10:21:02 +0300

    Update page.tsx

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -25,9 +25,8 @@ const breadcrumbItems = [
     },
 ];
 
-const Products = async (/* { searchParams }: PageProps */) => {
+const Products = async ({ searchParams }: PageProps) => {
     const currency = getServerCookie("currency") as CurrencyType;
-    const searchParams: any = {};
 
     const categorySlugs = searchParams.categories
         ? searchParams.categories.split(",")
