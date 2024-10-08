8a92411c06231d72955e294ae26d4900950492ab
Danylo <107757498+DanyloDiachenko@users.noreply.github.com>
2024-10-08 22:11:42 +0300

    _

--- store/slices/productsSettings/index.ts
+++ store/slices/productsSettings/index.ts
@@ -1,5 +1,5 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
-import { ProductsSettingsState } from "./productsFiltersState.interface";
+import { ProductsSettingsState } from "./productsSettingsState.interface";
 import {
     ProductBrand,
     ProductCategoryWithProductsQuantity,
