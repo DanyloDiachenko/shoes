ce8fbe11ae5b1bcddafb49227ae95ab41b7fdca7
Danylo <107757498+DanyloDiachenko@users.noreply.github.com>
2024-10-09 22:53:11 +0300

    _

--- components/pageComponents/Product/StickyProduct/index.tsx
+++ components/pageComponents/Product/StickyProduct/index.tsx
@@ -18,79 +18,16 @@ import { setCookie } from "@/helpers/setCookie";
 import { getCookieProductsClient } from "@/helpers/getCookieProductsClient";
 import { Quantity } from "./Quantity";
 import { getProductPrice } from "@/helpers/getProductPrice";
+import { AddProductToCart } from "@/components/additional/AddProductToCart";
 
 export const StickyProduct = ({
     product,
     currency,
     cookieProducts,
 }: StickyProductProps) => {
-    const dispatch = useDispatch();
-    const localStorageToogler = useSelector(
-        (state: RootState) => state.toogleLocalStorage.value
-    );
     const productToCart = useSelector(
         (state: RootState) => state.productToCart
     );
-
-    const [cookieProductsClient, setCookieProductsClient] =
-        useState<ProductCookie[]>(cookieProducts);
-
-    const toogleLocalStorageHandler = () => {
-        dispatch(toogleLocalStorage());
-    };
-    const setProductToCartHandler = (productToCart: ProductToCartState) => {
-        dispatch(setProductToCart(productToCart));
-    };
-
-    const onAddToCartClick = () => {
-        if (cookieProductsClient.find((p) => p.id === product.id)) {
-            toast.error("Product already in cart");
-            return;
-        }
-
-        if (!productToCart.size) {
-            toast.error("Please select size");
-            return;
-        }
-
-        const newCookieProducts: ProductCookie[] = [
-            ...cookieProductsClient,
-            {
-                id: product.id,
-                quantity: productToCart.quantity,
-                size: productToCart.size,
-            },
-        ];
-
-        setCookie("cart", JSON.stringify(newCookieProducts));
-        setCookieProductsClient(newCookieProducts);
-        toogleLocalStorageHandler();
-
-        setProductToCartHandler({
-            quantity: 1,
-            size: null,
-        });
-
-        toast.success("Product successfully added to cart");
-    };
-
-    const onRemoveProductClick = () => {
-        const updatedProducts = cookieProductsClient.filter(
-            (p) => p.id !== product.id
-        );
-
-        setCookie("cart", JSON.stringify(updatedProducts));
-        setCookieProductsClient(updatedProducts);
-        toogleLocalStorageHandler();
-
-        toast.success("Product successfully removed from cart");
-    };
-
-    useEffect(() => {
-        const cookieProducts = getCookieProductsClient();
-
-        setCookieProductsClient(cookieProducts);
-    }, [localStorageToogler]);
 
     return (
         <div className={styles.stickyProduct}>
@@ -121,28 +58,10 @@ export const StickyProduct = ({
                         </div>
                         <Quantity product={product} />
                         <div className={styles.actions}>
-                            <Button
-                                colorType="btnOutlinePrimary2"
-                                className={`${styles.addToCart}`}
-                                onClick={
-                                    cookieProductsClient.find(
-                                        (productCookie) =>
-                                            productCookie.id === product.id
-                                    )
-                                        ? onRemoveProductClick
-                                        : onAddToCartClick
-                                }
-                            >
-                                <LiaCartPlusSolid />
-                                <span>
-                                    {cookieProductsClient.find(
-                                        (productCookie) =>
-                                            productCookie.id === product.id
-                                    )
-                                        ? "Remove from cart"
-                                        : "Add to cart"}
-                                </span>
-                            </Button>
+                            <AddProductToCart
+                                product={product}
+                                cookieProducts={cookieProducts}
+                            />
                             <Button
                                 className={styles.addToWishlist}
                                 title="Add to wishlist"
