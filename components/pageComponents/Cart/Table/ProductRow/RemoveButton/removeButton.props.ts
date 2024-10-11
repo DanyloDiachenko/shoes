3fe88599f988532ba6f81a17f7b8423ef6e921b8
Danylo <107757498+DanyloDiachenko@users.noreply.github.com>
2024-10-11 21:22:04 +0300

    _

--- components/pageComponents/Cart/Table/ProductRow/RemoveButton/removeButton.props.ts
+++ components/pageComponents/Cart/Table/ProductRow/RemoveButton/removeButton.props.ts
@@ -1,5 +1,5 @@
-import { IProduct } from "@/interfaces/product.interface";
+import { Product } from "@/interfaces/product.interface";
 
 export interface RemoveButtonProps {
-    cartProduct: IProduct;
+    cartProduct: Product;
 }
