7b382240bbb05c2369464d616dfdb1f050711c96
Danylo <107757498+DanyloDiachenko@users.noreply.github.com>
2024-10-11 20:58:59 +0300

    _

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -2,14 +2,13 @@ import { Breadcrumbs } from "@/components/common/Breadcrumbs";
 import { PageHeader } from "@/components/common/PageHeader";
 import { CartPageContent } from "@/components/pageComponents/Cart";
 import { getCookieProductsServer } from "@/helpers/getCookieProductsServer";
-import { IProduct } from "@/interfaces/product.interface";
-import { IProductCookie } from "@/interfaces/productCookie.interface";
+import { Product } from "@/interfaces/product.interface";
+import { ProductCookie } from "@/interfaces/productCookie.interface";
 import { getProduct } from "../api/products";
-import { getServerCookie } from "@/helpers/getServerCookie";
-import { CurrencyType } from "@/types/currency.type";
 import { getCurrency } from "@/helpers/getCurrency";
+import { Breadcrumb } from "@/interfaces/breadcrumb.interface";
 
-const breadcrumbItems = [
+const breadcrumbs: Breadcrumb[] = [
     {
         title: "Home",
         link: "/",
@@ -25,10 +24,10 @@ const breadcrumbItems = [
 ];
 
 const Cart = async () => {
-    const cookieProducts: IProductCookie[] = getCookieProductsServer();
+    const cookieProducts: ProductCookie[] = getCookieProductsServer() || [];
     const currency = getCurrency();
 
-    let cartProducts: IProduct[] = [];
+    let cartProducts: Product[] = [];
 
     for (let i = 0; i < cookieProducts.length; i++) {
         const productToCart = await getProduct(cookieProducts[i].id);
@@ -39,7 +38,7 @@ const Cart = async () => {
     return (
         <>
             <PageHeader title="Shopping Cart" subtitle="Shop" />
-            <Breadcrumbs links={breadcrumbItems} />
+            <Breadcrumbs breadcrumbs={breadcrumbs} />
             <CartPageContent
                 cartProducts={cartProducts}
                 cookieProducts={cookieProducts}
