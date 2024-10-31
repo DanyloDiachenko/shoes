49eab74634eb3d9778edc45b6b869249dd86876d
Danylo <107757498+DanyloDiachenko@users.noreply.github.com>
2024-11-01 00:01:20 +0200

    form values

--- components/popups/ChangeAddress/index.tsx
+++ components/popups/ChangeAddress/index.tsx
@@ -6,20 +6,95 @@ import styles from "./styles.module.scss";
 import { Button } from "@/components/UI/Button";
 import { IoIosArrowRoundForward, IoMdClose } from "react-icons/io";
 import { usePathname } from "next/navigation";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { getProfile } from "@/app/api/auth/user";
 import { getClientCookie } from "@/helpers/getClientCookie";
+import { User } from "@/interfaces/user.inteface";
+import { UnathorizedResponse } from "@/interfaces/responses/unathorized.interface";
+
+interface FormValues {
+    firstName: string | null;
+    lastName: string | null;
+    phone: string | null;
+    email: string;
+    street: string | null;
+    city: string | null;
+    country: string | null;
+    postIndex: string | null;
+}
 
 export const ChangeAddress = ({}: ChangeAddressProps) => {
+    const [popupTitle, setPopupTitle] = useState<string>("");
+    const [fields, setFields] = useState<FormValues>();
+
+    const isUserProfile = (
+        profile: User | UnathorizedResponse
+    ): profile is User => {
+        return (profile as User).id !== undefined;
+    };
+
     const getUserProfile = async () => {
-        const token = await getClientCookie("token");
+        const token = getClientCookie("token");
 
         if (!token) {
             return;
         }
 
         const profile = await getProfile(token);
-        console.log(profile);
+
+        if (!isUserProfile(profile)) {
+            return;
+        }
+
+        if (profile.billingAddress) {
+            setPopupTitle("Change Billing Address");
+            setFields({
+                firstName: profile.firstName,
+                lastName: profile.lastName,
+                phone: profile.phone,
+                email: profile.email,
+                street: profile.billingAddress?.street,
+                city: profile.billingAddress?.city,
+                country: profile.billingAddress?.country,
+                postIndex: profile.billingAddress?.postIndex,
+            });
+        } else if (profile.shippingAddress) {
+            setPopupTitle("Change Shipping Address");
+            setFields({
+                firstName: profile.firstName,
+                lastName: profile.lastName,
+                phone: profile.phone,
+                email: profile.email,
+                street: profile.shippingAddress?.street,
+                city: profile.shippingAddress?.city,
+                country: profile.shippingAddress?.country,
+                postIndex: profile.shippingAddress?.postIndex,
+            });
+        } else if (!profile.billingAddress) {
+            setPopupTitle("Create Billing Address");
+            setFields({
+                firstName: profile.firstName,
+                lastName: profile.lastName,
+                phone: profile.phone,
+                email: profile.email,
+                street: "",
+                city: "",
+                country: "",
+                postIndex: "",
+            });
+        } else if (!profile.shippingAddress) {
+            setPopupTitle("Create Shipping Address");
+            setFields({
+                firstName: profile.firstName,
+                lastName: profile.lastName,
+                phone: profile.phone,
+                email: profile.email,
+                street: "",
+                city: "",
+                country: "",
+                postIndex: "",
+            });
+        }
     };
 
     useEffect(() => {
@@ -28,16 +103,26 @@ export const ChangeAddress = ({}: ChangeAddressProps) => {
 
     return (
         <div className={styles.formBox}>
-            <div className={styles.title}>Change Address</div>
+            <div className={styles.title}>{popupTitle}</div>
             <div className={styles.content}>
                 <form>
                     <div className={styles.formGroup}>
-                        <label htmlFor="address-name">User Name</label>
+                        <label htmlFor="address-firstName">First Name</label>
                         <Input
                             type="text"
-                            id="address-name"
-                            name="address-name"
-                            value="User Name"
+                            id="address-firstName"
+                            name="address-firstName"
+                            value="User First Name"
+                            disabled
+                        />
+                    </div>
+                    <div className={styles.formGroup}>
+                        <label htmlFor="address-firstName">Last Name</label>
+                        <Input
+                            type="text"
+                            id="address-lastName"
+                            name="address-lastName"
+                            value="User Last Name"
                             disabled
                         />
                     </div>
