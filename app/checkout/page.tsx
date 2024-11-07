6f9cafc7257cbc86f10fae45663373243ac09a73
Danylo <107757498+DanyloDiachenko@users.noreply.github.com>
2024-11-07 17:13:07 +0200

    checkout page

---
+++ app/checkout/page.tsx
@@ -0,0 +1,31 @@
+import { Breadcrumbs } from "@/components/common/Breadcrumbs";
+import { PageHeader } from "@/components/common/PageHeader";
+import { CheckoutPageComponent } from "@/components/pageComponents/Checkout";
+import { Breadcrumb } from "@/interfaces/breadcrumb.interface";
+
+const breadcrumbs: Breadcrumb[] = [
+    {
+        title: "Home",
+        link: "/",
+    },
+    {
+        title: "Shop",
+        link: "/products",
+    },
+    {
+        title: "Checkout",
+        link: "#",
+    },
+];
+
+const Checkout = () => {
+    return (
+        <>
+            <PageHeader title="Checkout" subtitle="Shop" />
+            <Breadcrumbs breadcrumbs={breadcrumbs} />
+            <CheckoutPageComponent />;
+        </>
+    );
+};
+
+export default Checkout;
