4dc0c0869af2f504a8c97e0fd637770230d2ab4d
Danylo <107757498+DanyloDiachenko@users.noreply.github.com>
2024-11-14 23:56:19 +0200

    form

--- components/pageComponents/Checkout/Form/index.tsx
+++ components/pageComponents/Checkout/Form/index.tsx
@@ -1,65 +1,100 @@
+"use client";
+
 import { Input } from "@/components/UI/Input";
 import styles from "./styles.module.scss";
 import { Checkbox } from "@/components/UI/Checkbox";
 import { Textarea } from "@/components/UI/Textarea";
 import { FormProps } from "./form.props";
+import { useState } from "react";
 
 export const Form = ({ user }: FormProps) => {
+    const [shippingToDifferentAddress, setShippingToDifferentAddress] =
+        useState(false);
+
     return (
         <div className={styles.column}>
             <h2 className={styles.title}>Shipping Details</h2>
             <div className="row">
                 <div className={styles.col}>
                     <label>First Name *</label>
-                    <Input type="text" disabled value={user.firstName || ""} />
+                    <Input
+                        type="text"
+                        disabled={!shippingToDifferentAddress}
+                        value={user.firstName || ""}
+                    />
                 </div>
-
                 <div className={styles.col}>
                     <label>Last Name *</label>
-                    <Input type="text" disabled />
+                    <Input
+                        type="text"
+                        disabled={!shippingToDifferentAddress}
+                        value={user.lastName || ""}
+                    />
                 </div>
             </div>
-            <label>Company Name (Optional)</label>
-            <Input type="text" />
             <label>Country *</label>
-            <Input type="text" disabled />
-            <label>Street address *</label>
             <Input
                 type="text"
-                placeholder="House number and Street name"
-                disabled
+                disabled={!shippingToDifferentAddress}
+                value={user.shippingAddress?.country || ""}
             />
-            <Input
-                type="text"
-                placeholder="Appartments, suite, unit etc ..."
-                disabled
-            />
+
             <div className="row">
                 <div className={styles.col}>
                     <label>Town / City *</label>
-                    <Input type="text" disabled />
+                    <Input
+                        type="text"
+                        disabled={!shippingToDifferentAddress}
+                        value={user.shippingAddress?.city || ""}
+                    />
                 </div>
                 <div className={styles.col}>
-                    <label>State / County *</label>
-                    <Input type="text" disabled />
+                    <label>Street *</label>
+                    <Input
+                        type="text"
+                        disabled={!shippingToDifferentAddress}
+                        value={user.shippingAddress?.street || ""}
+                    />
                 </div>
             </div>
             <div className="row">
                 <div className={styles.col}>
-                    <label>Postcode / ZIP *</label>
-                    <Input type="text" disabled />
+                    <label>Home number *</label>
+                    <Input
+                        type="text"
+                        disabled={!shippingToDifferentAddress}
+                        value={user.shippingAddress?.homeNumber || ""}
+                    />
                 </div>
                 <div className={styles.col}>
-                    <label>Phone *</label>
-                    <Input type="text" disabled />
+                    <label>Postcode / ZIP *</label>
+                    <Input
+                        type="text"
+                        disabled={!shippingToDifferentAddress}
+                        value={user.shippingAddress?.postIndex || ""}
+                    />
                 </div>
             </div>
+            <label>Phone *</label>
+            <Input
+                type="phone"
+                disabled={!shippingToDifferentAddress}
+                value={user.phone || ""}
+            />
             <label>Email address *</label>
-            <Input type="email" disabled />
+            <Input
+                type="email"
+                disabled={!shippingToDifferentAddress}
+                value={user.email || ""}
+            />
             <Checkbox
                 title="Ship to a different address?"
                 id="checkout-diff-address"
                 className={styles.checkbox}
+                onChange={() =>
+                    setShippingToDifferentAddress(!shippingToDifferentAddress)
+                }
+                value={String(shippingToDifferentAddress)}
             />
             <label className={styles.textareaLabel}>
                 Order notes (optional)
