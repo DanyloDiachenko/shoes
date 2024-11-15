b5b4bc88a82682780d5a5d0781c6440795c99afe
Danylo <107757498+DanyloDiachenko@users.noreply.github.com>
2024-11-15 23:47:01 +0200

    styles

--- components/pageComponents/Checkout/Form/index.tsx
+++ components/pageComponents/Checkout/Form/index.tsx
@@ -6,36 +6,26 @@ import { Checkbox } from "@/components/UI/Checkbox";
 import { Textarea } from "@/components/UI/Textarea";
 import { FormProps } from "./form.props";
 import { useState } from "react";
+import Link from "next/link";
 
 export const Form = ({ user }: FormProps) => {
-    const [shippingToDifferentAddress, setShippingToDifferentAddress] =
-        useState(false);
-
     return (
         <div className={styles.column}>
             <h2 className={styles.title}>Shipping Details</h2>
             <div className="row">
                 <div className={styles.col}>
                     <label>First Name *</label>
-                    <Input
-                        type="text"
-                        disabled={!shippingToDifferentAddress}
-                        value={user.firstName || ""}
-                    />
+                    <Input type="text" disabled value={user.firstName || ""} />
                 </div>
                 <div className={styles.col}>
                     <label>Last Name *</label>
-                    <Input
-                        type="text"
-                        disabled={!shippingToDifferentAddress}
-                        value={user.lastName || ""}
-                    />
+                    <Input type="text" disabled value={user.lastName || ""} />
                 </div>
             </div>
             <label>Country *</label>
             <Input
                 type="text"
-                disabled={!shippingToDifferentAddress}
+                disabled
                 value={user.shippingAddress?.country || ""}
             />
 
@@ -44,7 +34,7 @@ export const Form = ({ user }: FormProps) => {
                     <label>Town / City *</label>
                     <Input
                         type="text"
-                        disabled={!shippingToDifferentAddress}
+                        disabled
                         value={user.shippingAddress?.city || ""}
                     />
                 </div>
@@ -52,7 +42,7 @@ export const Form = ({ user }: FormProps) => {
                     <label>Street *</label>
                     <Input
                         type="text"
-                        disabled={!shippingToDifferentAddress}
+                        disabled
                         value={user.shippingAddress?.street || ""}
                     />
                 </div>
@@ -62,7 +52,7 @@ export const Form = ({ user }: FormProps) => {
                     <label>Home number *</label>
                     <Input
                         type="text"
-                        disabled={!shippingToDifferentAddress}
+                        disabled
                         value={user.shippingAddress?.homeNumber || ""}
                     />
                 </div>
@@ -70,32 +60,16 @@ export const Form = ({ user }: FormProps) => {
                     <label>Postcode / ZIP *</label>
                     <Input
                         type="text"
-                        disabled={!shippingToDifferentAddress}
+                        disabled
                         value={user.shippingAddress?.postIndex || ""}
                     />
                 </div>
             </div>
             <label>Phone *</label>
-            <Input
-                type="phone"
-                disabled={!shippingToDifferentAddress}
-                value={user.phone || ""}
-            />
+            <Input type="phone" disabled value={user.phone || ""} />
             <label>Email address *</label>
-            <Input
-                type="email"
-                disabled={!shippingToDifferentAddress}
-                value={user.email || ""}
-            />
-            <Checkbox
-                title="Ship to a different address?"
-                id="checkout-diff-address"
-                className={styles.checkbox}
-                onChange={() =>
-                    setShippingToDifferentAddress(!shippingToDifferentAddress)
-                }
-                value={String(shippingToDifferentAddress)}
-            />
+            <Input type="email" disabled value={user.email || ""} />
+            <Link href="#" className={styles.link}>Ship to a different address?</Link>
             <label className={styles.textareaLabel}>
                 Order notes (optional)
             </label>
