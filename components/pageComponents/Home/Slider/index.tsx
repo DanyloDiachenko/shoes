c6fb7c1384db2ff9a8fd9db1531e00e3d920a50a
Danylo <107757498+DanyloDiachenko@users.noreply.github.com>
2024-11-20 23:03:53 +0200

    _

--- components/pageComponents/Home/Slider/index.tsx
+++ components/pageComponents/Home/Slider/index.tsx
@@ -2,40 +2,42 @@
 
 import SlickSlider from "react-slick";
 import styles from "./styles.module.scss";
+import { Button } from "@/components/UI/Button";
+import Link from "next/link";
 
 export const Slider = () => {
     return (
         <div className="container">
             <SlickSlider className={styles.slider}>
-                <div className="intro-slide">
-                    <figure className="slide-image">
+                <div className={styles.slide}>
+                    <figure className={styles.image}>
                         <picture>
                             <source
                                 media="(max-width: 480px)"
-                                srcSet="assets/images/demos/demo-10/slider/slide-1-480w.jpg"
+                                srcSet="/media/banners/slider-1.png"
                             />
                             <img
-                                src="assets/images/demos/demo-10/slider/slide-1.jpg"
+                                src="/media/banners/slider-1.png"
                                 alt="Image Desc"
                             />
                         </picture>
                     </figure>
 
-                    <div className="intro-content">
-                        <h3 className="intro-subtitle">Deals and Promotions</h3>
-                        <h1 className="intro-title text-white">
+                    <div className={styles.content}>
+                        <h3 className={styles.subtitle}>
+                            Deals and Promotions
+                        </h3>
+                        <h1 className={styles.text}>
                             Sneakers & Athletic Shoes
                         </h1>
 
-                        <div className="intro-price text-white">from $9.99</div>
-
-                        <a
-                            href="category.html"
-                            className="btn btn-white-primary btn-round"
-                        >
-                            <span>SHOP NOW</span>
-                            <i className="icon-long-arrow-right"></i>
-                        </a>
+                        <div className={styles.price}>from $9.99</div>
+                        <Link href="/products">
+                            <Button colorType="btnPrimary">
+                                <span>SHOP NOW</span>
+                                <i className="icon-long-arrow-right"></i>
+                            </Button>
+                        </Link>
                     </div>
                 </div>
             </SlickSlider>
