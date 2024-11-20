b16701aebdef1c19a82a20fa7bfcc7375ab6c534
Danylo <107757498+DanyloDiachenko@users.noreply.github.com>
2024-11-20 22:56:46 +0200

    _

--- components/pageComponents/Home/Slider/index.tsx
+++ components/pageComponents/Home/Slider/index.tsx
@@ -1,5 +1,12 @@
+import SlickSlider from "react-slick";
 import styles from "./styles.module.scss";
 
 export const Slider = () => {
-    return <div className="container"></div>;
+    return (
+        <div className="container">
+            <SlickSlider className={styles.slider}>
+                <div className={styles.slide}></div>
+            </SlickSlider>
+        </div>
+    );
 };
