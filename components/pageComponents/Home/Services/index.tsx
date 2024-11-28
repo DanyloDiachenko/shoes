e452bbfc0903a5956d7fc3317690036eef0a082e
Danylo <107757498+DanyloDiachenko@users.noreply.github.com>
2024-11-29 00:22:14 +0200

    services styles

--- components/pageComponents/Home/Services/index.tsx
+++ components/pageComponents/Home/Services/index.tsx
@@ -1,20 +1,49 @@
+import { Service } from "./service.interface";
 import styles from "./styles.module.scss";
+import { LiaRocketSolid, LiaLifeRingSolid } from "react-icons/lia";
+import { MdRotateLeft } from "react-icons/md";
+import { AiOutlineInfoCircle } from "react-icons/ai";
+
+const services: Service[] = [
+    {
+        title: "Free Shipping",
+        description: "Orders $50 or more",
+        icon: <LiaRocketSolid />,
+    },
+    {
+        title: "Free Returns",
+        description: "Within 30 days",
+        icon: <MdRotateLeft />,
+    },
+    {
+        title: "Get 20% Off 1 Item",
+        description: "when you sign up",
+        icon: <AiOutlineInfoCircle />,
+    },
+    {
+        title: "We Support",
+        description: "24/7 amazing services",
+        icon: <LiaLifeRingSolid />,
+    },
+];
 
 export const Services = () => {
     return (
         <div className={`container ${styles.services}`}>
             <div className="row">
-                <div className={styles.column}>
-                    <div className={styles.iconBox}>
-                        <span className={styles.icon}>
-                            <svg></svg>
-                        </span>
-                        <div className={styles.content}>
-                            <h3 className={styles.title}>Free Shipping</h3>
-                            <p>Orders $50 or more</p>
+                {services.map((service, index) => (
+                    <div className={styles.column} key={index}>
+                        <div className={styles.iconBox}>
+                            <span className={styles.icon}>{service.icon}</span>
+                            <div className={styles.content}>
+                                <h3 className={styles.title}>
+                                    {service.title}
+                                </h3>
+                                <p>{service.description}</p>
+                            </div>
                         </div>
                     </div>
-                </div>
+                ))}
             </div>
         </div>
     );
