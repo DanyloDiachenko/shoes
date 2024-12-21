c0098e9a61b22df3634167ea29916c9e9a4a5bc2
Danylo <107757498+DanyloDiachenko@users.noreply.github.com>
2024-12-21 21:21:13 +0200

    styles

--- components/pageComponents/Home/TopSellingProducts/index.tsx
+++ components/pageComponents/Home/TopSellingProducts/index.tsx
@@ -54,101 +54,91 @@ export const TopSellingProducts = () => {
                     role="tabpanel"
                     aria-labelledby="top-all-link"
                 >
-                    <div className={styles.products}>
-                        <div className="row">
-                            <div className={styles.column}>
-                                <div className={styles.product}>
-                                    <figure className={styles.productMedia}>
-                                        <span className={styles.productLabel}>
-                                            Sale
-                                        </span>
-                                        <span className={styles.productLabel}>
-                                            30% off
-                                        </span>
-                                        <Link href={`/products/`}>
-                                            <img
-                                                src="https://nike.in.ua/image/cache/catalog/image/cache/catalog/image/catalog/image/nike/airmax/plus-tn/S-56201/32241-375x467.webp"
-                                                alt="Product image"
-                                                className={styles.productImage}
-                                            />
-                                        </Link>
-                                        <div
-                                            className={
-                                                styles.productActionVertical
-                                            }
+                    <div className={`${styles.row} row`}>
+                        <div className={styles.column}>
+                            <div className={styles.product}>
+                                <figure className={styles.productMedia}>
+                                    <span className={styles.productLabel}>
+                                        Sale
+                                    </span>
+                                    <span className={styles.productLabel}>
+                                        30% off
+                                    </span>
+                                    <Link href={`/products/`}>
+                                        <img
+                                            src="https://nike.in.ua/image/cache/catalog/image/cache/catalog/image/catalog/image/nike/airmax/plus-tn/S-56201/32241-375x467.webp"
+                                            alt="Product image"
+                                            className={styles.productImage}
+                                        />
+                                    </Link>
+                                    <div
+                                        className={styles.productActionVertical}
+                                    >
+                                        <Button
+                                            colorType="btnOutlinePrimary2"
+                                            className={styles.addToWishlist}
                                         >
-                                            <Button
-                                                colorType="btnOutlinePrimary2"
-                                                className={styles.addToWishlist}
-                                            >
-                                                <span className="sr-only">
-                                                    add to wishlist
-                                                </span>
-                                            </Button>
-                                        </div>
-                                    </figure>
-                                    <div className={styles.productBody}>
-                                        <div
-                                            className={styles.productCategories}
-                                        >
-                                            <Link href="#">Men’s</Link>,
-                                            <Link href="#">Sneakers</Link>
-                                        </div>
-                                        <h3 className={styles.productTitle}>
-                                            <Link href={`/products/${1}`}>
-                                                ASICS Tiger Gel-Lyte MT
-                                            </Link>
-                                        </h3>
-                                        <div className={styles.productPrice}>
-                                            <span className={styles.newPrice}>
-                                                Now $77.99
+                                            <span className="sr-only">
+                                                add to wishlist
                                             </span>
-                                            <span className={styles.oldPrice}>
-                                                $130.00
-                                            </span>
-                                        </div>
+                                        </Button>
                                     </div>
-                                    <div className={styles.productFooter}>
-                                        <div
-                                            className={styles.ratingsContainer}
+                                </figure>
+                                <div className={styles.productBody}>
+                                    <div className={styles.productCategories}>
+                                        <Link href="#">Men’s</Link>,
+                                        <Link href="#">Sneakers</Link>
+                                    </div>
+                                    <h3 className={styles.productTitle}>
+                                        <Link href={`/products/${1}`}>
+                                            ASICS Tiger Gel-Lyte MT
+                                        </Link>
+                                    </h3>
+                                    <div className={styles.productPrice}>
+                                        <span className={styles.newPrice}>
+                                            Now $77.99
+                                        </span>
+                                        <span className={styles.oldPrice}>
+                                            $130.00
+                                        </span>
+                                    </div>
+                                </div>
+                                <div className={styles.productFooter}>
+                                    <div className={styles.ratingsContainer}>
+                                        <div className={styles.ratings}>
+                                            {getProductRating(4)}
+                                        </div>
+                                        <span className={styles.ratingsText}>
+                                            ( 4 Reviews )
+                                        </span>
+                                    </div>
+                                    <div className={styles.productNav}>
+                                        <Link
+                                            href="#"
+                                            style={{
+                                                background: "#af5f23",
+                                            }}
                                         >
-                                            <div className={styles.ratings}>
-                                                {getProductRating(4)}
-                                            </div>
-                                            <span
-                                                className={styles.ratingsText}
-                                            >
-                                                ( 4 Reviews )
+                                            <span className="sr-only">
+                                                Color name
                                             </span>
-                                        </div>
-                                        <div className={styles.productNav}>
-                                            <Link
-                                                href="#"
-                                                style={{
-                                                    background: "#af5f23",
-                                                }}
-                                            >
-                                                <span className="sr-only">
-                                                    Color name
-                                                </span>
-                                            </Link>
-                                        </div>
-                                        <div className={styles.productAction}>
-                                            <Link
-                                                href={`/products/1`}
-                                                className={styles.addToCart}
-                                                title="Add to cart"
-                                            >
-                                                <span>add to cart</span>
-                                            </Link>
-                                            <Link
-                                                href="#"
-                                                className={styles.quickView}
-                                                title="Quick view"
-                                            >
-                                                <span>quick view</span>
-                                            </Link>
-                                        </div>
+                                        </Link>
+                                    </div>
+                                    <div className={styles.productAction}>
+                                        <Link
+                                            href={`/products/1`}
+                                            className={styles.addToCart}
+                                            title="Add to cart"
+                                        >
+                                            <span>add to cart</span>
+                                        </Link>
+                                        <Link
+                                            href="#"
+                                            className={styles.quickView}
+                                            title="Quick view"
+                                        >
+                                            <span>quick view</span>
+                                        </Link>
                                     </div>
                                 </div>
                             </div>
