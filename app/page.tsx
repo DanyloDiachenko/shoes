36b8b2d2a3fdba2c1f63377acef3e65fcb203509
Danylo <107757498+DanyloDiachenko@users.noreply.github.com>
2024-12-27 22:18:36 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import { HomePageComponent } from "@/components/pageComponents/Home";
-
+import { getCategories } from "./api/categories";
 
 const Home = async () => {
     const categories = await getCategories();
