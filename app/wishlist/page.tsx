9f4e7338804bd2c873336469a7b33379b6fd955e
Danylo <107757498+DanyloDiachenko@users.noreply.github.com>
2025-01-05 14:51:53 +0200

    breadcrumbs

--- app/wishlist/page.tsx
+++ app/wishlist/page.tsx
@@ -1,9 +1,27 @@
+import { Breadcrumbs } from "@/components/common/Breadcrumbs";
 import { PageHeader } from "@/components/common/PageHeader";
+import { Breadcrumb } from "@/interfaces/breadcrumb.interface";
+
+const breadcrumbs: Breadcrumb[] = [
+    {
+        title: "Home",
+        link: "/",
+    },
+    {
+        title: "Shop",
+        link: "/shop",
+    },
+    {
+        title: "Wishlist",
+        link: "/wishlist",
+    },
+];
 
 const Wishlist = () => {
     return (
         <>
             <PageHeader title="Wishlist" subtitle="Shop" />
+            <Breadcrumbs breadcrumbs={breadcrumbs} />
         </>
     );
 };
