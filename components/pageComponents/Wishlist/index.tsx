f68db10ce9386da210b1f08920ae673e998d9979
Danylo <107757498+DanyloDiachenko@users.noreply.github.com>
2025-01-05 15:25:48 +0200

    table start

--- components/pageComponents/Wishlist/index.tsx
+++ components/pageComponents/Wishlist/index.tsx
@@ -1,10 +1,61 @@
+import Link from "next/link";
 import styles from "./styles.module.scss";
+import { Button } from "@/components/UI/Button";
 
 export const WishlistPageComponent = () => {
     return (
-        <div className={styles.pageContent}>
+        <div className="page-content">
             <div className="container">
-                <table className={styles.table}></table>
+                <table className={styles.table}>
+                    <thead>
+                        <tr>
+                            <th>Product</th>
+                            <th>Price</th>
+                            <th>Stock Status</th>
+                            <th></th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td className={styles.productCol}>
+                                <div className={styles.product}>
+                                    <figure className={styles.productMedia}>
+                                        <Link href="#">
+                                            <img
+                                                src="https://nike.in.ua/image/cache/catalog/image/cache/catalog/image/catalog/image/nike/airmax/plus-tn/S-56201/32241-375x467.webp"
+                                                alt="Product image"
+                                            />
+                                        </Link>
+                                    </figure>
+
+                                    <h3 className={styles.productTitle}>
+                                        <Link href="#">
+                                            Blue utility pinafore denim dress
+                                        </Link>
+                                    </h3>
+                                </div>
+                            </td>
+                            <td className={styles.priceCol}>$76.00</td>
+                            <td className={styles.stockCol}>
+                                <span className={styles.inStock}>In stock</span>
+                            </td>
+                            <td className={styles.actionCol}>
+                                <Button
+                                    colorType="btnPrimary"
+                                    className={styles.button}
+                                >
+                                    icon Add to Cart
+                                </Button>
+                            </td>
+                            <td className={styles.removeCol}>
+                                <button className={styles.removeButton}>
+                                    icon
+                                </button>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
                 <div className={styles.wishlistShare}></div>
             </div>
         </div>
