6af8fdc53a8debd586ed998c62e6022bd029802d
Danylo <107757498+DanyloDiachenko@users.noreply.github.com>
2025-02-02 23:10:34 +0200

    background img fix

--- components/pageComponents/Login/index.tsx
+++ components/pageComponents/Login/index.tsx
@@ -3,12 +3,7 @@ import styles from "./styles.module.scss";
 
 export const LoginPageComponent = () => {
     return (
-        <div
-            className={styles.loginPage}
-            style={{
-                backgroundImage: "url('/images/banners/login-bg.png')",
-            }}
-        >
+        <div className={styles.loginPage}>
             <div className="container">
                 <FormBox />
             </div>
