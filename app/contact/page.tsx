b179902618931f09bf5454f934ecc64387941e00
Danylo <107757498+DanyloDiachenko@users.noreply.github.com>
2025-04-11 11:23:57 +0300

    contact

---
+++ app/contact/page.tsx
@@ -0,0 +1,29 @@
+import { Breadcrumbs } from "@/components/common/Breadcrumbs";
+import { ContactPageComponent } from "@/components/pageComponents/Contact";
+import { Breadcrumb } from "@/interfaces/breadcrumb.interface";
+
+const breadcrumbs: Breadcrumb[] = [
+    {
+        title: "Home",
+        link: "/",
+    },
+    {
+        title: "Pages",
+        link: "/",
+    },
+    {
+        title: "Contact Us",
+        link: "/contact",
+    },
+];
+
+const Contact = () => {
+    return (
+        <>
+            <Breadcrumbs breadcrumbs={breadcrumbs} />
+            <ContactPageComponent />
+        </>
+    );
+};
+
+export default Contact;
---
+++ components/pageComponents/Contact/index.tsx
@@ -0,0 +1,9 @@
+
+
+export const ContactPageComponent = () => {
+    return (
+        <>
+            
+        </>
+    );
+};
